8af62e6815f9d63396d8eab219c329019d094996
Tarcísio Marinho <tarcisio_marinho09@hotmail.com>
2017-05-31 23:29:28 -0300

    Update download.py

--- download.py
+++ download.py
@@ -7,7 +7,6 @@ import requests
 import bs4 as bs
 import lxml
 import os
-import youtube_dl
 
 BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
 
